a9c90f662c826741bd3ea829419a38ca603134de
TakácsMártonandrás <marton.takacs2@stud.uni-corvinus.hu>
2022-11-23 13:55:03 +0100

    final version

---
+++ WebApplication1/wwwroot/geek.js
@@ -0,0 +1,22 @@
+﻿var viccek;
+
+function letöltés() {
+    fetch('/jokes.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data))
+}
+
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+    var geek = document.getElementById("geek")
+    for (var i = 0; i < viccek.length; i++) {
+        var újelem = document.createElement("div")
+        újelem.innerHTML = viccek[i].question
+        geek.appendChild(újelem)
+    }
+}
+window.onload = letöltés();
+console.log(viccek)
---
+++ WebApplication1/wwwroot/jsgyak.js
@@ -0,0 +1,34 @@
+﻿window.onload = () => {
+    console.log("betöltődött")
+}
+var faktor = n => {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    else {
+        return n * faktor(n - 1)
+    }
+}
+
+function pascalharomszog(sor, oszlop) {
+    szam = faktor(sor) / (faktor(oszlop) * faktor(sor - oszlop))
+    return szam
+}
+
+var pascal = document.getElementById("pascal")
+var méret = 10
+for (var s = 0; s < méret; s++) {
+    var újsor = document.createElement("div")
+    újsor.classList.add("sor")
+    //újsor.innerHTML = faktor(s)
+    pascal.appendChild(újsor)
+
+    for (var o = 0; o <= s; o++) {
+        var újelem = document.createElement("div")
+        újelem.innerHTML = pascalharomszog(s, o);
+        //újelem.innerHTML = `${s}:${o}`;
+        újelem.classList.add("elem")
+        újsor.appendChild(újelem)
+    }
+}
+
