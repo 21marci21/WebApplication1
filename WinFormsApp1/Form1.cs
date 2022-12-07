76cc76b0ed14fe8ff3175555a19fe3d72778e05d
TakácsMártonandrás <marton.takacs2@stud.uni-corvinus.hu>
2022-12-07 14:32:50 +0100

    week12v2

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -113,5 +113,22 @@ namespace WinFormsApp1
             CreateExcel(); 
             CreateTable();
         }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
     }
 }
